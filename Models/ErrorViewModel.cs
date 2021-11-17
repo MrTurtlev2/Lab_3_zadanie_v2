2bd6690d1f2054bf551a3e824b678c26d147a599
Marek <wojnarmarek0@gmail.com>
2021-11-17 21:41:59 +0100

    Dodaj pliki projektów.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Lab_3_zadanie_v2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
